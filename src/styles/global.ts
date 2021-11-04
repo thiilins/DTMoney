f6477f69e94841a799adb1bff05a502fc8377605
Thiago Lins <taglins@gmail.com>
2021-11-04 20:26:26 -0300

    Estrutura da aplicação - 06

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,7 +24,13 @@ html{
    font-size: 87.5%  //14px
  }
 }
-
+body, input, textarea,button{
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+}
+h1, h2, h3, h4, h5, h6, strong{
+  font-weight: 600;
+}
 body{
   background: var(--background);
    -webkit-font-smoothing: antialiased;                                     
