d46c48882efb5213d5e45415e464e7963bf2f8f5
Thiago Lins <taglins@gmail.com>
2021-11-04 22:57:30 -0300

    Componentização - 03 [Transaction Table]

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,44 @@
+import { Container } from "./style";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titúlo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Site</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>13/04/2021</td>
+          </tr>
+          <tr>
+            <td>Hamburguer</td>
+            <td className="withdraw">-R$ 59,00</td>
+            <td>Alimentação</td>
+            <td>20/04/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Site</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>13/04/2021</td>
+          </tr>
+          <tr>
+            <td>Hamburguer</td>
+            <td className="withdraw">-R$ 59,00</td>
+            <td>Alimentação</td>
+            <td>20/04/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
