5d3429940b6c6cdfc92be0c5c28b0d616f56b373
Thiago Lins <taglins@gmail.com>
2021-11-05 13:50:11 -0300

    Modal e Forms - 01/02

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,29 @@
+import Modal from "react-modal";
+import { Container } from "./style";
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+Modal.setAppElement("#root");
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input placeholder="Titúlo" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
