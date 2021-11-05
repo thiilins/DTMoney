95dbea9d78a168c2f65f4258eefc992674a42604
Thiago Lins <taglins@gmail.com>
2021-11-05 02:10:32 -0300

    Consumindo API - 2 [ Configurando MirageJS]

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
 
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return {
+        id: 1,
+        title: "Transaction 1",
+        amount: 6000,
+        type: "deposit",
+        category: "Salário",
+        date: new Date("2021-12-10"),
+      };
+    });
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
